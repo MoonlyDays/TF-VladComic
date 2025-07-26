8ca13594d4fc106368e486f2d6300939a1250ac6
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2025-07-26 23:51:31 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -61,8 +61,8 @@ function App() {
 
         setCurrentImageUrl(comicImageUrl(currentPage));
         const preloadImages = range(0, PRELOAD_AHEAD)
-            .map(n => comicImageUrl(currentPage + n))
-            .filter(url => url !== undefined) as string[];
+            .map((n) => comicImageUrl(currentPage + n))
+            .filter((url) => url !== undefined) as string[];
         setPreloadImageUrls(preloadImages);
     }, [currentPage]);
 
@@ -85,72 +85,60 @@ function App() {
                 <img src={TF2Logo} alt="Team Fortress 2 Logo" />
                 <img src={Title} alt="The Days Have Worn Away" />
             </div>
-            <div className="h-[48rem] w-full overflow-hidden relative" onClick={nextPage}>
+            <div
+                className="relative h-[86rem] w-full overflow-hidden"
+                onClick={nextPage}
+            >
                 {initialLoading && (
                     <>
                         <Transition show={imageLoading}>
-                            <div
-                                className="absolute z-10 duration-400 transition w-full h-full pointer-events-none bg-black bg-black/40 data-closed:bg-transparent flex justify-center items-center">
+                            <div className="duration-400 data-closed:bg-transparent pointer-events-none absolute z-10 flex h-full w-full items-center justify-center bg-black bg-black/40 transition">
                                 <AiOutlineLoading3Quarters
-                                    className={'text-6xl text-white transition-opacity animate-spin ' + (loadingVisible ? '' : 'opacity-0')} />
+                                    className={
+                                        'animate-spin text-6xl text-white transition-opacity ' +
+                                        (loadingVisible ? '' : 'opacity-0')
+                                    }
+                                />
                             </div>
                         </Transition>
 
                         {currentImageUrl && (
                             <img
                                 onLoad={handleLoaded}
-                                className="object-contain w-full h-full cursor-pointer"
+                                className="h-full w-full cursor-pointer object-contain"
                                 alt="Comic Reader"
                                 src={currentImageUrl}
                             />
                         )}
 
-                        {preloadImageUrls && preloadImageUrls.map((url, index) => (
-                            <img
-                                key={index}
-                                alt="Hidden Preload"
-                                className="w-0 h-0"
-                                src={url}
-                            />
-                        ))}
+                        {preloadImageUrls &&
+                            preloadImageUrls.map((url, index) => (
+                                <img
+                                    key={index}
+                                    alt="Hidden Preload"
+                                    className="h-0 w-0"
+                                    src={url}
+                                />
+                            ))}
                     </>
                 )}
             </div>
 
             <div className="mb-16 mt-8 flex flex-col items-center gap-8 text-white">
-                <div
-                    className="flex h-[72px] w-[600px] items-center justify-center bg-[url(./assets/infobar.png)] font-sans text-xl">
+                <div className="flex h-[72px] w-[600px] items-center justify-center bg-[url(./assets/infobar.png)] font-sans text-xl">
                     Жмите на картинку или на пробел для перехода дальше
                 </div>
 
                 <div className="flex flex-col gap-4 text-center">
-                    <div>
-                        <p>
-                            Оригинал:{' '}
-                            <a
-                                href="https://www.teamfortress.com/tf07_thedayshavewornaway/"
-                                className="text-amber-500"
-                            >
-                                teamfortress.com/tf07_thedayshavewornaway
-                            </a>
-                            . Авторы перевода: Alabes,{' '}
-                            <a href="https://t.me/moonlygroup" className="text-amber-500">
-                                Moonly Days
-                            </a>
-                            .
-                        </p>
-                        <p>
-                            Это фанатский перевод. Мы не связаны с Valve Corporation или
-                            порталом &quot;Авторский Комикс&quot;.
-                        </p>
-                    </div>
                     <div className="text-gray-400">
+                        <p>&copy; No Mood No Friends, 2025</p>
+                        <p>&copy; Moonly Days, издание и платформа, 2025</p>
                         <p>
-                            &copy; Valve Corporation, Team Fortress 2, комикс &quot;The Days Have
-                            Worn Away&quot;, 2024
+                            &copy; Valve Corporation, all rights reserved.
+                            Valve, the Valve logo, Steam, the Steam logo, Team
+                            Fortress, the Team Fortress logo are trademarks
+                            and/or registered trademarks of Valve Corporation.
                         </p>
-                        <p>&copy; Alabes, перевод текстов, 2024</p>
-                        <p>&copy; Moonly Days, издание и платформа, 2024</p>
                     </div>
                 </div>
             </div>
